1ff3c17089dbf2f72bc3ade713922979da1c0ff9
Luis Cabrera Benito <parzibyte@gmail.com>
2024-05-03 09:38:06 -0600

    Comandos para versión, ayuda e impresoras

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ fs.readFile(nombreArchivoPreferencias, (err, data) => {
 const guardarPreferencias = (cb) => {
     fs.writeFile(nombreArchivoPreferencias, JSON.stringify(idsUsuarioConImpresoraPreferida), cb);
 }
+
+const obtenerVersion = async () => {
+    try {
+        const urlInvocacion = `http://localhost:8000/version`;
+        const httpResponse = await fetch(urlInvocacion);
+        return await httpResponse.json();
+    } catch (e) {
+        return e.message;
+    }
+}
 const obtenerImpresoras = async () => {
     try {
         const urlInvocacion = `http://localhost:8000/impresoras`;
@@ -158,6 +168,19 @@ const responderConImpresoras = async (idChat) => {
     }
 }
 
+
+const responderConVersion = async (idChat) => {
+    const version = await obtenerVersion();
+    if (typeof version !== "string" && version.version) {
+        await bot.sendMessage(idChat, `Versión:  *${version.version}*
+Plataforma: *${version.plataforma}*
+Sistema operativo: *${version.sistemaOperativo}*`, { parse_mode: "Markdown" });
+    } else {
+
+        await bot.sendMessage(idChat, "Error obteniendo versión: " + impresoras);
+    }
+}
+
 const evaluarRespuestaYEnviarMensajeAUsuario = async (respuestaDelPlugin, idChat) => {
     if (respuestaDelPlugin === true) {
         await bot.sendMessage(idChat, "Impreso correctamente");
@@ -165,6 +188,31 @@ const evaluarRespuestaYEnviarMensajeAUsuario = async (respuestaDelPlugin, idChat
         await bot.sendMessage(idChat, "Error: " + respuestaDelPlugin);
 
     }
+}
+
+const mostrarAyuda = async (idChat) => {
+    await bot.sendMessage(idChat, `*Configurando plugin*
+1. Necesitas ejecutar el plugin y configurarlo como se indica en: https://parzibyte.me/blog/
+2. Luego, elige tu impresora predeterminada con /impresoras, solo necesitas hacerlo una vez y a partir de ahí se usará la misma impresora para todas las operaciones
+
+*Modo de uso*
+
+- Envía texto sin formato y será impreso en la impresora seleccionada como texto normal
+- Envía un enlace de una página web *que no cargue recursos externos* y será impresa como una imagen
+- Envía una foto (como foto, no como archivo) y será impresa
+- Envía código HTML formateado con \`\`\`html
+El código va aquí\`\`\` y será impreso como una página HTML
+
+- Envía un arreglo de operaciones JSON formateado con \`\`\`json
+Las operaciones van acá\`\`\` y serán impresas como si estuvieras
+invocando al plugin desde un lenguaje de programación. Docs: https://gist.github.com/parzibyte/2f36655ef9d6ea8e6de73c6e09bbc735#file-documentacion-txt
+
+El ancho máximo para todas las imágenes impresas es ${MAXIMO_ANCHO} es y el algoritmo seleccionado es ${ALGORITMO_RASTER_BIT_IMAGE}, pero puede ser 0, 1 o 2. Mira: https://parzibyte.me/blog/2024/01/23/actualizacion-plugin-esc-pos-v3-3-0-algoritmos-imagenes/
+
+También puedes usar los siguientes comandos: 
+/impresoras elige la impresora preferida para el usuario
+/version muestra la versión del plugin. Útil para conocer el estado de ejecución del plugin
+/ayuda muestra este mensaje de ayuda`, { parse_mode: "Markdown" });
 }
 
 bot.on('message', async (msg) => {
@@ -194,12 +242,19 @@ bot.on('message', async (msg) => {
             } else if (entidad.type === "url") {
                 await evaluarRespuestaYEnviarMensajeAUsuario(
                     await descargarHtmlEImprimir(impresoraPreferida, msg.text), idChat);
+            } else if (entidad.type === "bot_command") {
+                if (contenidoDeLaEntidad === "/version") {
+                    await responderConVersion(idChat);
+                } else if (contenidoDeLaEntidad === "/impresoras") {
+                    await responderConImpresoras(idChat);
+                } else if (contenidoDeLaEntidad === "/ayuda") {
+                    await mostrarAyuda(idChat);
+                }
             }
         }
         return;
     }
     if (msg.text) {
-        
         await evaluarRespuestaYEnviarMensajeAUsuario(
             await escribirTexto(impresoraPreferida, msg.text),
             idChat,
